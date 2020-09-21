76fe278098a2aa14c5ee7a3772593a8fe06ae155
Abel Beak <abel@teamuni.net>
2020-09-22 00:42:25 +0900

    first

---
+++ src/main/java/net/teamuni/mobmanager/Mobmanager.java
@@ -0,0 +1,17 @@
+package net.teamuni.mobmanager;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Mobmanager extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
