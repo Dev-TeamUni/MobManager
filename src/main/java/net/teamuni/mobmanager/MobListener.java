1c2ad2699ebe6db92db34277fbc61091bf866084
CeylonPark <ceylonp20@gmail.com>
2020-12-03 22:09:52 +0900

    Add world spawn prevent

--- src/main/java/net/teamuni/mobmanager/MobListener.java
+++ src/main/java/net/teamuni/mobmanager/MobListener.java
@@ -25,6 +25,19 @@ public class MobListener implements Listener {
 
     @EventHandler
     public void onSpawnCreature(CreatureSpawnEvent event) {
+        //월드별 스폰 제한 확인
+        String world = event.getLocation().getWorld().getName();
+        if(this.config.isSet(world)) {
+            String reason = world + ".MOB_SPAWN_REASON." + event.getSpawnReason().name();
+            String spawn = world + ".MOB_SPAWN." + event.getEntityType().name();
+            if(this.config.isBoolean(spawn)) {
+                event.setCancelled(!this.config.getBoolean(spawn));
+            }
+            if(this.config.isBoolean(reason)) {
+                event.setCancelled(!this.config.getBoolean(reason));
+            }
+        }
+
         //몹 상황별 스폰 제한
         if(event.getEntity() instanceof Mob
                 && this.config.getBoolean("MOB_SPAWN_REASON"+event.getSpawnReason().name())) {
